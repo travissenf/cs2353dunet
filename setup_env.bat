55cf219b20f408cd5bc21c84bb57cd0919bb09e6
Travy Senf <tsenf@stanford.edu>
2025-05-29 12:17:33 -0700

    data

---
+++ setup_env.bat
@@ -0,0 +1,5 @@
+@echo off
+setx nnUNet_raw_data_base "C:/Users/Travis/vscode projects/trycode/kaggle_tcga-lgg/nnUNet_raw_data_base"
+setx nnUNet_preprocessed "C:/Users/Travis/vscode projects/trycode/kaggle_tcga-lgg/nnUNet_preprocessed"
+setx RESULTS_FOLDER "C:/Users/Travis/vscode projects/trycode/kaggle_tcga-lgg/nnUNet_trained_models"
+echo Environment variables have been set. Please restart your terminal for the changes to take effect. 
